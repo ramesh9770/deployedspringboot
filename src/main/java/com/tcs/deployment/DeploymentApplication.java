dccb7a0f31325ccc92caf71eafbcf5033052807c
ramesh_java <ramesh_java@outlook.com>
2022-11-23 10:39:25 +0530

    first deployement

---
+++ src/main/java/com/tcs/deployment/DeploymentApplication.java
@@ -0,0 +1,14 @@
+package com.tcs.deployment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeploymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeploymentApplication.class, args);
+		System.out.println("its me");
+	}
+
+}
